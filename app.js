64b9cf6af9b0522c22a3f1d8094a042cebe98062
circlefour <0100.circle@gmail.com>
2025-05-10 19:20:37 -0400

    basic chat handling failure and message recovery

--- app.js
+++ app.js
@@ -3,46 +3,86 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-// create an express app
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
-
-console.log(io);
-
-// define a route for the home page
-app.get('/', (req, res) => {
-    //res.send('sup bitch');
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
-//app.get('/socket.io/socket.io.js', (req, res) => {
-//  res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.js');
-//});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg, msg2) => {
-        io.emit('chat message', msg);
-        //console.log(msg2);
-        socket.broadcast.emit('chat message', msg);
-        console.log('message: ' + msg);
+// to await setup tasks before starting the server
+async function main() {
+    // open database file
+    const db = await open({
+        filename: 'chat.db',
+        driver: sqlite3.Database
     });
-});
 
-io.engine.on("connection_error", (err) => {
-    console.log(err.req);
-    console.log(err.code);
-    console.log(err.message);  // the error message, for example "Session ID unknown"
-    console.log(err.context);  // some additional error context
-});
+    // create our 'messages' table (you can ignore the 'client_offset' column for now)
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_offset TEXT UNIQUE,
+            content TEXT
+        );
+    `);
 
-// have the server listen on specified port
-server.listen(3000, () => {
-    console.log("server is running on http://localhost:3000");
-});
+    // create an express app
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {}
+    });
+    
+    // define a route for the home page
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'index.html'));
+    });
+    
+    io.on('connection', async (socket) => {
+        console.log('a user connected');
+        socket.on('chat message', async (msg) => {
+            let result;
+            try {
+                // store the message in the database
+                result = await db.run ('INSERT INTO messages (content) VALUES (?)', msg);
+            } catch (e) {
+                // TODO: handle failure
+                console.error('Failed to insert message:', e);
+                return;
+            }
+            // include the offset with the message
+            io.emit('chat message', msg, result.lastID);
+        });
+    
+        if (!socket.recovered) {
+            // if the connection state recovery was not successful
+            try {
+                await db.each('SELECT id, content FROM messages WHERE id > ?',
+                    [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit('chat message', row.content, row.id);
+                    }
+                )
+            } catch (e) {
+                // something went wrong
+            }
+        }
+    });
+    
+    io.engine.on("connection_error", (err) => {
+        console.log(err.req);
+        console.log(err.code);
+        console.log(err.message);  // the error message, for example "Session ID unknown"
+        console.log(err.context);  // some additional error context
+    });
+    
+    // have the server listen on specified port
+    // server.listen instead of app.listen because socket.io needs to attach itself to a raw http port, not express
+    server.listen(3001, () => {
+        console.log("server is running on http://localhost:3001");
+    });
+}
 
+//main();
+
+main().catch((err) => {
+  console.error('❌ Failed to start:', err);
+});
 
